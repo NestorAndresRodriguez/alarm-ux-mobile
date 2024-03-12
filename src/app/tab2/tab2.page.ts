84bae40d5fb4953ed58cfad4c7eef064d88e7332
NestorAndresRodriguez <na.rodriguez2@uniandes.edu.co>
2024-03-12 01:14:55 -0500

    Vista de timer

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
+  date: any = new Date();
+  now: any;
+  targetDate: Date = new Date(this.date+(30 * 60 * 1000));
+  targetTime: any = this.targetDate.getTime();
+  difference: number = 0;
+  months: Array<string> = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  currentTime: any = `${
+    this.months[this.targetDate.getMonth()]
+  } ${this.targetDate.getDate()}, ${this.targetDate.getFullYear()}`;
 
-  constructor() {}
+  @ViewChild('hours', { static: true }) hours: ElementRef;
+  @ViewChild('minutes', { static: true }) minutes: ElementRef;
+  @ViewChild('seconds', { static: true }) seconds: ElementRef;
+
+  ngOnInit() {
+    setInterval(() => {
+      this.tickTock();
+      this.difference = this.targetTime - this.now;
+      this.difference = this.difference / (1000 * 60 * 60 * 24);
+
+    }, 1000);
+  }
+
+  tickTock() {
+    this.date = new Date();
+    this.now = this.date.getTime();
+    this.hours.nativeElement.innerText = 0 - this.date.getHours();
+    this.minutes.nativeElement.innerText = 60 - this.date.getMinutes();
+    this.seconds.nativeElement.innerText = 60 - this.date.getSeconds();
+  }
+
+  detener(){
+    this.targetDate = new Date(this.date+(30 * 60 * 1000));
+    this.targetTime = this.targetDate.getTime();
+    setInterval(() => {
+      this.tickTock();
+      this.difference = this.targetTime - this.now;
+      this.difference = this.difference / (1000 * 60 * 60 * 24);
+
+    }, 1000);
+  }
+
+  
 
 }
